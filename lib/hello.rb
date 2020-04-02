33e59fadd7b74cce53a142ba253bb89ed9507547
DAZUN XING <dazun.xing@gmail.com>
2020-04-02 01:06:22 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,7 +5,12 @@ def hello_t(array)
     i=i+1
   end
 end
-hello_t
+
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
 
 # call your method here!
 
